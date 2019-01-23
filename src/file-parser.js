583988d186b3d02112e79160d4fdb02cd1efb879
0Calories <cppanand@gmail.com>
2019-01-23 14:48:52 -0500

    Add functionality for counting types of comments

--- src/file-parser.js
+++ src/file-parser.js
@@ -1,12 +1,12 @@
 // Analyzes a file by counting # of lines, # of comments, and # of TODOs
 const analyzeFile = file => {
   // Initialize variables for parsing the file
-  let numLines;
-  let numCommentLines;
-  let numSingleLineComments;
-  let numBlockCommentLines;
-  let numBlockComments;
-  let numTodos;
+  let numLines = 0;
+  let numCommentLines = 0;
+  let numSingleLineComments = 0;
+  let numBlockCommentLines = 0;
+  let numBlockComments = 0;
+  let numTodos = 0;
 
   let commentLines = [];
   let commentBlockFlag = false;
@@ -26,7 +26,7 @@ const analyzeFile = file => {
 
     // If the line starts with '/*', activate the commentBlockFlag so all subsequent lines are counted as comments
     // If the line ends with '*/', a comment block was escaped so disable the flag
-    if (line.substring(0, 2) === '/*' || line.includes('/*'))
+    if (line.substring(0, 2) === '/*' || line.includes('/*')) 
       commentBlockFlag = true;
     else if (line.substring(line.length - 2, line.length) == '*/')
       commentBlockFlag = false;
@@ -36,9 +36,30 @@ const analyzeFile = file => {
     } 
   });
 
-  console.log(`Number of lines: ${numLines}. Number of comment lines: ${commentLines.length}`);
+  // Parse the comment lines
+  commentLines.forEach(line => {
+
+    //const isTrailingComment = line.includes('//') || line.includes('/*') || line.includes('*/');
+
+    if (line.substring(0, 2) === '//' || line.includes('//')) 
+      numSingleLineComments++;
+    else if (line.substring(0, 2) == '/*') 
+      numBlockComments++;
+    
+    if (line.includes('TODO:'))
+      numTodos++;
+
+  });
+
   console.log(lines);
   console.log(commentLines);
+
+  console.log(`Total # of lines: ${numLines}`);
+  console.log(`Total # of comment lines: ${commentLines.length}`);
+  console.log(`Total # of single line comments: ${numSingleLineComments}`);
+  console.log(`Total # of comment lines within block comments: ${numBlockCommentLines}`);
+  console.log(`Total # of block comments: ${numBlockComments}`);
+  console.log(`Total # of TODO's: ${numTodos}`);
 };
 
 
